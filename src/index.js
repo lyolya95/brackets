bb9637aa4640c7c6d840d0e7113b7dd36647dd86
lyolya <Lelkabygr@mail.ru>
2021-01-28 21:58:53 +0400

    feat: added decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = bracketsConfig.flat();
+    let newArray = [];
+    const lengthStr = str.length;
+    for (let i = 0; i < bracketsConfig.flat().length / 2; i++) {
+        newArray.push(array.splice(0, 2).join(''))
+    }
+
+    for (let i = 0; i < lengthStr + 1; i++) {
+        while (str.includes(newArray[i])) {
+            str = str.replace(newArray[i], '');
+        }
+        newArray.push(newArray[i])
+    }
+
+    return !str;
 }
